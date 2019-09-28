c78eb048bfbb8e6e251a4c366462af557777c7c6
hchowdhury3273 <hc2324@nyu.edu>
2019-09-28 05:18:19 -0400

    contacts

--- ios/cfg/cfg/ContactTableViewCell.swift
+++ ios/cfg/cfg/ContactTableViewCell.swift
@@ -10,15 +10,20 @@ import UIKit
 
 class ContactTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var profileView: UIImageView!
+    @IBOutlet weak var pointsLabel: UILabel!
+    
+    
+    let images = ["profile", "profile copy", "profile copy 2", "profile copy 3"]
+    let points = ["328", "251", "86", "93", "107", "65"]
+    
+    func setContact(_ contact: Contact, _ indexPath: Int) {
+        nameLabel.text = contact.firstName + " " + contact.familyName
+        profileView.image = UIImage(named: images[indexPath%4])
+        pointsLabel.text = points[indexPath%6]
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    
 
 }
--- ios/cfg/cfg/ContactsViewController.swift
+++ ios/cfg/cfg/ContactsViewController.swift
@@ -7,24 +7,69 @@
 //
 
 import UIKit
+import Contacts
 
 class ContactsViewController: UIViewController {
-
+    @IBOutlet weak var contactListTableView: UITableView!
+    var contactList: [Contact] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        contactList = createContactList()
+        
+        contactListTableView.delegate = self
+        contactListTableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func createContactList() -> [Contact]{
+        var tempContacts: [Contact] = []
+        let store = CNContactStore()
+        
+        store.requestAccess(for: .contacts) { (granted, err) in
+            if let err = err {
+                print("failed to req acess", err)
+                return//this is completion handeling. prolly more bootstrap way of doing this too
+            }
+            if granted {
+                let keys = [CNContactGivenNameKey, CNContactFamilyNameKey]
+                let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
+                do{
+                    try store.enumerateContacts(with: request, usingBlock: {(contact, stopPointerIfYouWantStopEnumerating) in
+                        let setContact = Contact(firstName: contact.givenName, familyName: contact.familyName)
+                        //                        setContact.delegate = self
+                        tempContacts.append(setContact)
+                    })
+                } catch let err{ print("enumeration fail", err) }
+                
+            }
+            else { print("acess denied") }
+        }
+        return tempContacts
     }
-    */
-
 }
+
+extension ContactsViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contactList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currCell = contactList[indexPath.row]
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "contactCellID", for: indexPath) as? ContactTableViewCell else{
+            print("cellforrowat dont work")
+            fatalError()
+        }
+        cell.setContact(currCell, indexPath.row)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //do this when tapped on cell
+        
+    }
+    
+}
+
