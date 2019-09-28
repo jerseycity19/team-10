11777c93917ef477a521ab11ac7052bf7a2213e9
eric <eric@leapis.dev>
2019-09-28 03:58:21 -0400

    added /hosts endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 
+var MongoClient = require('mongodb').MongoClient
 var router = express.Router()
 
-router.get('/hosts', async (req, res) => {
-
+app.get('/hosts', async (req, res) => {
+    var dbName = "cfg";
+    var url =  "mongodb://songenwang.com:27017";
+    const client = new MongoClient(url);
+    client.connect(function(err) {
+        const db = client.db(dbName);
+        db.collection("hosts").find({time:"10/2/19 at 2:20pm"}, function (err, doc) {
+            doc.toArray( function(err, data){
+                console.log(data);
+            });
+        });
+        client.close();
+    });
 });
 
-router.get('/:id', function(req, res) { });
+router.get('/user/:id', function(req, res) { 
+    console.log("found");
+});
 
 app.get('/', function (req, res) {
  return res.send('{"return":"none"}');
