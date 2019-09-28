cc9b479252629d16c1da7b4b880be55e933d096e
hchowdhury3273 <hc2324@nyu.edu>
2019-09-27 22:16:46 -0400

    login text

--- ios/cfg/cfg/LoginViewController.swift
+++ ios/cfg/cfg/LoginViewController.swift
@@ -7,26 +7,38 @@
 //
 
 import UIKit
-import Firebase
+//import Firebase
 import GoogleSignIn
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        
+        
+        // Automatically sign in the user.
+        GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+        //        performSegue(withIdentifier: "MapViewController", sender: nil)
+        // ...
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!,
+                withError error: NSError!) {
+        if (error == nil) {
+            // Perform any operations on signed in user here.
+            // ...
+            self.performSegue(withIdentifier: "MapViewController", sender: nil)
+            
+            
+        } else {
+            //            print("\(error.localizedDescription)")
+            
+        }
     }
-    */
 
 }
