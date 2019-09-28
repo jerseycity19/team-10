c1bf683f19c23a40015056cceb968e5419344fd5
hchowdhury3273 <hc2324@nyu.edu>
2019-09-28 05:12:49 -0400

    star

---
+++ ios/cfg/cfg/ContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactTableViewCell.swift
+//  cfg
+//
+//  Created by Helal Chowdhury on 9/28/19.
+//  Copyright © 2019 Helal. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ios/cfg/cfg/ContactsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactsViewController.swift
+//  cfg
+//
+//  Created by Helal Chowdhury on 9/28/19.
+//  Copyright © 2019 Helal. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
