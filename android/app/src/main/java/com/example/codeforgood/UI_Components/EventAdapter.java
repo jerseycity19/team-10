61b9317c82424babfe3d0807a4e3dceb42cb667c
Logan Le <loggannlee@gmail.com>
2019-09-28 04:40:20 -0400

    Added more features for EventAdapter

--- android/app/src/main/java/com/example/codeforgood/UI_Components/EventAdapter.java
+++ android/app/src/main/java/com/example/codeforgood/UI_Components/EventAdapter.java
@@ -1,11 +1,14 @@
 package com.example.codeforgood.UI_Components;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.codeforgood.R;
 import com.example.codeforgood.models.MeetingEvent;
 
@@ -17,31 +20,52 @@ import androidx.recyclerview.widget.RecyclerView;
 public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> {
     private Context context;
     private List<MeetingEvent> meetings;
-    @NonNull
-    @Override
-    public EventAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
 
-    @Override
-    public void onBindViewHolder(@NonNull EventAdapter.ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView monthtv, dateTv;
         private ImageView imageView;
 
-        public ViewHolder(View itemView){
+        public ViewHolder(View itemView) {
             super(itemView);
             monthtv = itemView.findViewById(R.id.month_id);
             dateTv = itemView.findViewById(R.id.date);
             imageView = itemView.findViewById(R.id.eventPosterId);
         }
+
+        void bind(MeetingEvent meetingEvent) {
+            monthtv.setText(meetingEvent.getMonth());
+            dateTv.setText(meetingEvent.getDate());
+//            Glide.with(context).load(String.format("https://image.tmdb.org/t/p/w342%s", movie.getPosterPath())).into(ivPoster);
+
+//            container.setOnClickListener(v -> {
+//                Intent intent = new Intent(context, DetailActivity.class);
+//                intent.putExtra(MVBUNDLE, movie);
+//                context.startActivity(intent);
+//            });
+        }
     }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.event_item_row, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        MeetingEvent movie = meetings.get(position);
+        viewHolder.bind(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (meetings != null) {
+            return meetings.size();
+        } else {
+            return 0;
+        }
+    }
+
 }
