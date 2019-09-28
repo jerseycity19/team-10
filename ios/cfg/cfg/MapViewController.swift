7781d57e8722b2e08b3dbe3bfccad4a5f317e557
hchowdhury3273 <hc2324@nyu.edu>
2019-09-27 23:36:37 -0400

    didload

--- ios/cfg/cfg/MapViewController.swift
+++ ios/cfg/cfg/MapViewController.swift
@@ -13,22 +13,24 @@ import FlyoverKit
 import Speech
 
 class MapViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    let locationManager = CLLocationManager()
+    var currentCoordinate: CLLocationCoordinate2D?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        mapView.delegate = self
+        userLocationSetup()
+        self.mapSetup()
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
