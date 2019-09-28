2a36381c065cbc211b8650bcf836977bb32ec9d6
hchowdhury3273 <hc2324@nyu.edu>
2019-09-28 04:10:43 -0400

    calendar

--- ios/cfg/cfg/DetailsViewController.swift
+++ ios/cfg/cfg/DetailsViewController.swift
@@ -13,6 +13,7 @@ import MapKit
 
 class DetailsViewController: UIViewController, MKMapViewDelegate {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,14 +21,32 @@ class DetailsViewController: UIViewController, MKMapViewDelegate {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonClicked(_ sender: Any) {
+        let eventStore:EKEventStore = EKEventStore()
+        
+        eventStore.requestAccess(to: .event) {(granted, error) in
+            if (granted) && (error) == nil
+            {
+                print("granted \(granted)")
+                print("error \(error)")
+                
+                let event:EKEvent = EKEvent(eventStore: eventStore)
+                event.title = "Mental Health Meeting"
+                event.startDate = Date()
+                event.endDate = Date()
+                event.notes = ""
+                event.calendar = eventStore.defaultCalendarForNewEvents
+                do {
+                    try eventStore.save(event, span: .thisEvent)
+                } catch let error as NSError{
+                    print("error : \(error)")
+                }
+                print("Save Event")
+            } else{
+                print("error : \(error)")
+            }
+            
+        }
     }
-    */
-
+    
 }
