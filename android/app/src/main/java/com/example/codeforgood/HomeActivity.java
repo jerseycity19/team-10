19a60cb6e79a43052ee53398bcf6155abff0624b
Jia Yu Lin <purpleinklingyu@gmail.com>
2019-09-27 23:25:42 -0400

    updated home page xml

--- android/app/src/main/java/com/example/codeforgood/HomeActivity.java
+++ android/app/src/main/java/com/example/codeforgood/HomeActivity.java
@@ -14,22 +14,6 @@ import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
